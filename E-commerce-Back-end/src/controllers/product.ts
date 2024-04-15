bd3859fdf586a0ce068ca4fdac6b6b0e41c4abcd
Pratham <prathamlashkari841@gmail.com>
2024-04-15 23:53:15 +0530

    Creating get product apis

--- E-commerce-Back-end/src/controllers/product.ts
+++ E-commerce-Back-end/src/controllers/product.ts
@@ -22,7 +22,7 @@ export const addProduct =TryCatch(async(req:Request<{},{},NewProducttype>,res,ne
          })
          return next(new ErrorHandler("Please enter all fields",404))
       }
-   const product = Product.create({
+   const product = await Product.create({
     name,
     price,
     category:category.toLowerCase(),
@@ -49,9 +49,64 @@ export const getLatestProduct =TryCatch(async(req:Request<{},{},NewProducttype>,
 
 export const getAllCategories =TryCatch(async(req:Request<{},{},NewProducttype>,res,next)=>{
 
-   const product = await Product.find({}).sort({createdAt : -1}).limit(5);
+   const category = await Product.distinct("category");
+   return res.status(201).send({
+    success : true,
+    category
+   })
+});
+
+
+
+
+export const getAdminProduct =TryCatch(async(req:Request<{},{},NewProducttype>,res,next)=>{
+
+   const product = await Product.find({});
    return res.status(201).send({
     success : true,
     product
+   })
+});
+
+
+
+export const getSingleProduct =TryCatch(async(req,res,next)=>{
+
+   const product = await Product.findById(req.params.id);
+   return res.status(201).send({
+    success : true,
+    product
+   })
+});
+
+
+export const updateProduct =TryCatch(async(req,res,next)=>{
+
+   const id = req.params.id;
+   const {name, stock,category,price} = req.body;
+   const photo = req.file;
+   let product = await Product.findById(id);
+   if(!photo)
+      {
+         return next(new ErrorHandler("Please enter photo",404));
+      }
+
+   if(!name || !photo || !stock || !price)
+      {
+         rm(photo.path,()=>{
+         })
+         return next(new ErrorHandler("Please enter all fields",404))
+      }
+    product = await Product.create({
+    name,
+    price,
+    category:category.toLowerCase(),
+    stock,
+    photo:photo?.path
+   });
+
+   return res.status(201).send({
+    success : true,
+    message : "Product created"
    })
 });
--- E-commerce-Back-end/src/routes/product.ts
+++ E-commerce-Back-end/src/routes/product.ts
@@ -1,6 +1,6 @@
 import express  from "express";
 import { singleUpload } from "../middlewares/multer.js";
-import { addProduct, getAllCategories,getLatestProduct } from "../controllers/product.js";
+import { addProduct, getAdminProduct, getAllCategories,getLatestProduct, getSingleProduct } from "../controllers/product.js";
 import { adminOnly } from "../middlewares/auth.js";
 
 const app = express.Router();
@@ -14,4 +14,10 @@ app.get ("/latest",getLatestProduct);
 
 // For category
 app.get("/category" ,getAllCategories );
+
+// to get all products
+app.get("/admin-product",adminOnly,getAdminProduct);
+
+app.route("/:id").get(getSingleProduct);
+
 export default app;

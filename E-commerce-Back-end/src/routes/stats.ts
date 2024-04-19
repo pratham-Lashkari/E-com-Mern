fdf23189797f8f7fc906b75ce02d3821ac4457a2
Pratham <prathamlashkari841@gmail.com>
2024-04-19 22:37:56 +0530

    Adding routes

--- E-commerce-Back-end/src/routes/stats.ts
+++ E-commerce-Back-end/src/routes/stats.ts
@@ -1,18 +1,19 @@
 import express from "express";
+import { getBarCharts, getDashboardStats, getLineCharts, getPieCharts } from "../controllers/stats.js";
 
 const app = express.Router();
 
 // api/v1/dashboard/stats
-app.get("/stats");
+app.get("/stats" , getDashboardStats);
 
 // api/v1/dashboard/pie
-app.get("/pie");
+app.get("/pie",getPieCharts);
 
 // api/v1/dashboard/bar
-app.get("/bar");
+app.get("/bar" , getBarCharts);
 
 // api/v1/dashboard/line
-app.get("/line");
+app.get("/line" , getLineCharts);
 
 
 export default app;

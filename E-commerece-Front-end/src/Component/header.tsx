f8cc6bd9bfb856c5a51cf87e846df443cbccdd56
Pratham <prathamlashkari841@gmail.com>
2024-04-26 22:46:36 +0530

    Adding reducers

--- E-commerece-Front-end/src/Component/header.tsx
+++ E-commerece-Front-end/src/Component/header.tsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import { FaSearch, FaShoppingBag, FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 import { User } from '../types/types';
+import { signOut } from 'firebase/auth';
+import { auth } from '../firebase';
+import toast from 'react-hot-toast';
 
 interface PropType {
   user : User|null
@@ -10,8 +13,13 @@ export default function Header({user}:PropType) {
 
   const [isOpen , setisOpen] = useState<boolean>(false);
 
-  const logoutHandler =()=>{
-    setisOpen(false);
+  const logoutHandler = async()=>{
+    try {
+      await signOut(auth);
+      toast.success("Sign out successfully");
+    } catch (error) {
+      toast.error("Sign out Failed");
+    }
   }
   return (
     <nav className='header'>
@@ -35,7 +43,7 @@ export default function Header({user}:PropType) {
              </div>
           </dialog>
           </>
-        ):<Link to={"/login"} onClick={()=>(console.log(user))} ><FaSignInAlt/></Link>
+        ):<Link to={"/login"} onClick={()=>(logoutHandler)} ><FaSignInAlt/></Link>
       }
     </nav>
   )

2eb8facd1ddbf9360d9ecca5828b332f56d0d8f0
Pratham <prathamlashkari841@gmail.com>
2024-04-29 17:31:56 +0530

    Adding payemnt mode using stipe

--- E-commerce-Back-end/src/routes/payment.ts
+++ E-commerce-Back-end/src/routes/payment.ts
@@ -1,15 +1,19 @@
 import express from "express";
-import { applyDiscount, createPaymentIntent, deleteCupon, getAllCupons, newCupon } from "../controllers/cupon.js";
+import { 
+  applyDiscount, 
+  createPaymentIntent, 
+  deleteCupon, 
+  getAllCupons, 
+  newCupon } from "../controllers/cupon.js";
 import { adminOnly } from "../middlewares/auth.js";
 
 const app = express.Router();
 
-// For creating a cupon 
-app.post("/coupon/new",adminOnly ,newCupon );
-
-
 // For creating  payement 
 app.post("/create" , createPaymentIntent);
+
+// For creating a cupon 
+app.post("/coupon/new",adminOnly ,newCupon );
 
 // getting discount
 app.get("/discount" ,applyDiscount);
